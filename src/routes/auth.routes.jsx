f8ca22119a11da9c7ba70ca5e2753a4da4971c84
Pedro <pedronascimento.cst@gmail.com>
2025-02-23 22:10:07 +0000

    corrigindoIndex

--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import {SignIn} from "../pages/SignIn"
+import { SignIn } from "../pages/SignIn";
 import {SignUp} from "../pages/SignUp"
 
 export function AuthRoutes(){
